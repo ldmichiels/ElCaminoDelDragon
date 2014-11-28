8f66f97c55938586a577eeb9a5c15bdeaac85c86
Lucas <ldmichiels@gmail.com>
2014-11-27 22:55:32 -0300

    #10 Crear e Implementar la clase WeaponManager
    se sube lo basico, puede seguir creciendo.

---
+++ Source/engine/WeaponManager.hx
@@ -0,0 +1,54 @@
+package engine;
+
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import openfl.Assets;
+
+/**
+ * @author ldmichiels
+ */
+class WeaponManager extends GameElement {
+
+	private var weapons:Map<String, Weapon>;
+	private static var instance:WeaponManager = null;
+
+	private function new(){
+		super();
+		weapons = new Map<String, Weapon>();
+	}
+
+	public static function getInstance():WeaponManager {
+		if (instance == null)
+			instance = new WeaponManager();
+		return instance;
+	}
+
+	override public function updateLogic(time:Float) {
+		super.updateLogic(time);
+
+		for (key in weapons.keys()) {
+			var wpn = weapons.get(key);
+			if (wpn.isActive())
+				wpn.updateLogic(time);
+		}
+	}
+
+	public function getWeapon(name:String) {
+		return weapons.get(name);
+	}
+
+	public function setWeapon(name:String, wpn: Weapon) {
+		if (!weapons.exists(name)) 
+			weapons.set(name, wpn);
+		else 
+			//generar error
+			return false;
+		return true;
+	}
+
+	public function createWeapon(name:String, scene:GameScene) {
+		// @todo: hay q sacar esta constante
+		var wpn = new Weapon(scene, 10);
+		return this.setWeapon(name, wpn);
+	}
+}
